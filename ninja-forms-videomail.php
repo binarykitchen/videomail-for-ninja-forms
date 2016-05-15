a4f21687c5a042a0da4f9d4eb99a85bdcd901fcb
Kyle B. Johnson <kyle@wpninjas.com>
2016-05-15 19:25:00 -0400

    Initial commit. Generated with Ninja Forms Kozo.

---
+++ ninja-forms-videomail.php
@@ -0,0 +1,176 @@
+<?php if ( ! defined( 'ABSPATH' ) ) exit;
+
+/*
+ * Plugin Name: Ninja Forms - Videomail
+ * Plugin URI:
+ * Description: Express yourself in more than just words. Record and send a short video from your webcam.
+ * Version: 3.0.0
+ * Author: Kyle B. Johnson
+ * Author URI: http://kylebjohnson.me/
+ * Text Domain: ninja-forms-videomail
+ *
+ * Copyright 2016 Kyle B. Johnson.
+ */
+
+if( version_compare( get_option( 'ninja_forms_version', '0.0.0' ), '3.0', '>' ) || get_option( 'ninja_forms_load_deprecated', FALSE ) ) {
+
+    return;
+
+} else {
+
+    /**
+     * Class NF_Videomail
+     */
+    final class NF_Videomail
+    {
+        const VERSION = '3.0.0';
+        const SLUG    = 'videomail';
+        const NAME    = 'Videomail';
+        const AUTHOR  = 'Kyle B. Johnson';
+        const PREFIX  = 'NF_Videomail';
+
+        /**
+         * @var NF_Videomail
+         * @since 3.0
+         */
+        private static $instance;
+
+        /**
+         * Plugin Directory
+         *
+         * @since 3.0
+         * @var string $dir
+         */
+        public static $dir = '';
+
+        /**
+         * Plugin URL
+         *
+         * @since 3.0
+         * @var string $url
+         */
+        public static $url = '';
+
+        /**
+         * Main Plugin Instance
+         *
+         * Insures that only one instance of a plugin class exists in memory at any one
+         * time. Also prevents needing to define globals all over the place.
+         *
+         * @since 3.0
+         * @static
+         * @static var array $instance
+         * @return NF_Videomail Highlander Instance
+         */
+        public static function instance()
+        {
+            if (!isset(self::$instance) && !(self::$instance instanceof NF_Videomail)) {
+                self::$instance = new NF_Videomail();
+                self::$dir = plugin_dir_path(__FILE__);
+                self::$url = plugin_dir_url(__FILE__);
+                spl_autoload_register(array(self::$instance, 'autoloader'));
+            }
+            return self::$instance;
+        }
+
+        /**
+         * NF_Videomail constructor
+         */
+        public function __construct()
+        {
+            add_filter( 'ninja_forms_register_fields', array($this, 'register_fields'));
+            add_filter( 'ninja_forms_register_actions', array($this, 'register_actions'));
+        }
+
+        /**
+         * Register Fields
+         *
+         * @hook ninja_forms_register_fields
+         * @param $actions
+         * @return mixed
+         */
+        public function register_fields($actions)
+        {
+            $actions[ 'videomail' ] = new NF_Videomail_Fields_VideomailExample(); // includes/Fields/VideomailExample.php
+
+            return $actions;
+        }
+
+        /**
+         * Register Actions
+         *
+         * @hook ninja_forms_register_actions
+         * @param $actions
+         * @return mixed
+         */
+        public function register_actions($actions)
+        {
+            $actions[ 'videomail' ] = new NF_Videomail_Actions_VideomailExample(); // includes/Actions/VideomailExample.php
+
+            return $actions;
+        }
+
+        /**
+         * Autoloader
+         *
+         * @param $class_name
+         */
+        public function autoloader($class_name)
+        {
+            if (class_exists($class_name)) return;
+
+            if ( false === strpos( $class_name, self::PREFIX ) ) return;
+
+            $class_name = str_replace( self::PREFIX, '', $class_name );
+            $classes_dir = realpath(plugin_dir_path(__FILE__)) . DIRECTORY_SEPARATOR . 'includes' . DIRECTORY_SEPARATOR;
+            $class_file = str_replace('_', DIRECTORY_SEPARATOR, $class_name) . '.php';
+
+            if (file_exists($classes_dir . $class_file)) {
+                require_once $classes_dir . $class_file;
+            }
+        }
+        
+        /**
+         * Template
+         *
+         * @param string $file_name
+         * @param array $data
+         */
+        public static function template( $file_name = '', array $data = array() )
+        {
+            if( ! $file_name ) return;
+
+            extract( $data );
+
+            include self::$dir . 'includes/Templates/' . $file_name;
+        }
+        
+        /**
+         * Config
+         *
+         * @param $file_name
+         * @return mixed
+         */
+        public static function config( $file_name )
+        {
+            return include self::$dir . 'includes/Config/' . $file_name . '.php';
+        }
+    }
+
+    /**
+     * The main function responsible for returning The Highlander Plugin
+     * Instance to functions everywhere.
+     *
+     * Use this function like you would a global variable, except without needing
+     * to declare the global.
+     *
+     * @since 3.0
+     * @return {class} Highlander Instance
+     */
+    function NF_Videomail()
+    {
+        return NF_Videomail::instance();
+    }
+
+    NF_Videomail();
+}
