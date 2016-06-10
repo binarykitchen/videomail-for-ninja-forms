92e874203f4d94bda4723dc079aa47c961ad0ad6
Kyle B. Johnson <kyle@wpninjas.com>
2016-06-09 23:38:31 -0400

    copy over example for posting without email.

--- assets/js/videomail.js
+++ assets/js/videomail.js
@@ -5,16 +5,47 @@ var VideomailFieldController = Marionette.Object.extend({
     },
 
     register: function( model ) {
-        var VideomailClient = require('videomail-client'), // load videomail client package
-            videomailClient = new VideomailClient({        // instantiate with some options
-                verbose:       true,                         // prints additional info to console
-                disableSubmit: true                          // disable submissions to keep example simple
-            });
 
-        // this will load your webcam, fill the placeholder containing
-        // the `id="videomail"` with HTML and CSS code, place buttons and much more.
-        videomailClient.show();
-    }
+        var VideomailClient = require('videomail-client');
+
+        var videomailClient = new VideomailClient(
+            {
+                verbose: false,
+                video: {
+                    limitSeconds: 80,
+                    width: 240,
+                    countdown: false
+                }
+            }
+        );
+
+        var startOverButton = document.getElementById('startOver');
+
+        var onSubmitted = function(videomail) {
+
+            if( videomail[ 'url' ] ) {
+                model.set( 'url', videomail[ 'url' ] ); // Submit the Video URL with the form.
+                model.set( 'value', videomail[ 'url' ] );
+            }
+
+            if( videomail[ 'webm' ] ) {
+                model.set( 'webm', videomail[ 'webm' ] );
+            }
+
+            if( videomail[ 'mp4' ] ) {
+                model.set( 'mp4', videomail[ 'mp4' ] );
+            }
+
+            this.replay(videomail, 'viewVideo');
+            startOverButton.onclick = this.startOver
+        };
+
+        videomailClient.on(
+            videomailClient.events.SUBMITTED,
+            onSubmitted.bind( videomailClient )
+        );
+        videomailClient.show()
+    },
 
 });
 
