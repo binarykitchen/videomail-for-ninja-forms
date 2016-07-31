9b771b633dd57847a57472d5ef70c503b5584771
Kyle B. Johnson <kyle@wpninjas.com>
2016-07-30 21:34:00 -0400

    add top margin to buttons.

--- includes/Fields/Videomail.php
+++ includes/Fields/Videomail.php
@@ -56,6 +56,7 @@ class NF_Videomail_Fields_Videomail extends NF_Abstracts_Field
 
     public function enqueue_scripts()
     {
+        wp_enqueue_style( 'nf-videomail', NF_Videomail::$url . '/assets/css/videomail.css' );
         wp_enqueue_script( 'nf-videomail-client', NF_Videomail::$url . '/node_modules/videomail-client/dist/videomail-client.min.js', false );
         wp_enqueue_script( 'nf-videomail', NF_Videomail::$url . '/assets/js/min/videomail.min.js', array( 'nf-front-end', 'nf-videomail-client' ) );
     }
