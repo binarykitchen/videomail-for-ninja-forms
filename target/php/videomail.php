88434d07a66de1e0895d1467ecfee67e0f4cda53
Michael Heuberger <michael.heuberger@binarykitchen.com>
2018-07-12 18:25:39 +1200

    Final commit of version 3.6.6

--- target/php/videomail.php
+++ target/php/videomail.php
@@ -2,7 +2,7 @@
 final class NF_Videomail {
 
   const NAME = 'Videomail';
-  const VERSION = '3.6.5';
+  const VERSION = '3.6.6';
   const AUTHOR = 'Michael Heuberger + Kyle B. Johnson';
   const SLUG = 'videomail';
   const PREFIX = 'NF_Videomail';
