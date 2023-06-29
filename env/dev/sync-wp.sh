166b2fc6aa09f9733bdd15600f19f571ade7b703
Michael Heuberger <michael.heuberger@binarykitchen.com>
2023-06-29 19:32:07 +1200

    switch to checksum check

--- env/dev/sync-wp.sh
+++ env/dev/sync-wp.sh
@@ -4,6 +4,9 @@ set -eu
 
 cd "${0%/*}"
 
+// TODO consider replacing this script with this:
+// https://github.com/Pierre-Lannoy/wordpress-actions
+
 $PWD/wp-plugin-git-svn-sync/sync.sh \
   --plugin-name="videomail-for-ninja-forms" \
   --git-repo="https://github.com/binarykitchen/videomail-for-ninja-forms" \
