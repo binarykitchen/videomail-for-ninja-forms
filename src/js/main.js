cd727c71fab7a44aae340ffd37465ba47cc1f326
Michael Heuberger <michael.heuberger@binarykitchen.com>
2025-02-05 12:43:40 +1300

    Bump VC

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,5 @@
 // Remember, you can't use any import/require statements here
-const PLUGIN_VERSION = "7.0.4";
+const PLUGIN_VERSION = "7.0.5";
 
 // manual switch to have more stuff printed to console
 let DEBUG = false;
